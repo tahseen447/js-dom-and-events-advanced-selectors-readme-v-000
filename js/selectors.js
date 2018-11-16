b28a79b808ffbdb2f5798c070500abf05b1fd7cf
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-16 18:59:44 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img').last();
+}
+
+function ninjaBabySelector(){
+  return $('img#baby-ninja');
+}
